96c6bb0bf9e2cfdd0b3147acf9def8099bd33470
AliaksandrAliakseyenka <ali.aliaksandr@gmail.com>
2023-01-05 22:48:28 +0300

    next_step_java

--- first_step_learn_java/src/SearchInArray.java
+++ first_step_learn_java/src/SearchInArray.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class SearchInArray {
+public class SearchInArray {
+    public static void main(String[] args){
+        int[] array_one = {11,23,342,44,5,20,45};
+        int[] array_two = {112,324,43534,123,55,24,242,64432};
+
+        printMaxValue(array_two);
+    }
+
+    static void printMaxValue (int[] array) {
+        int max = array[0];
+        int maxIndex = 0;
+        int comparisions = 0;
+        int replacements = 0;
+        for (int i = 0; i < array.length; i++){
+            if(array[i]>max){
+                max = array[i];
+                maxIndex = i;
+                replacements++;
+            }
+            comparisions++;
+        }
+        System.out.println("Max element: " + max);
+        System.out.println("Max element index: " + maxIndex);
+        System.out.println("Comparisons: " + comparisions);
+        System.out.println("Replacements: " + replacements);
+    }
 }
