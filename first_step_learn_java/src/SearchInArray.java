5b88d2e0c25841654ca0c667da4d1b42a0ca61e3
AliaksandrAliakseyenka <ali.aliaksandr@gmail.com>
2023-01-05 23:13:28 +0300

    next_step_java

--- first_step_learn_java/src/SearchInArray.java
+++ first_step_learn_java/src/SearchInArray.java
@@ -1,18 +1,22 @@
 public class SearchInArray {
-    public static void main(String[] args){
-        int[] array_one = {11,23,342,44,5,20,45};
-        int[] array_two = {112,324,43534,123,55,24,242,64432};
+    public static void main(String[] args) {
 
-        printMaxValue(array_two);
+        int[] array_one = {11, 23, 342, 44, 5, 20, 45};
+        int[] array_two = {1, 5, 13, 20, 55, 66, 242, 64432};
+        printMaxValue(array_one);
+        int key = 20;
+        int position = binarySearch(array_two, key);
+        System.out.println(key + " position is " + position);
     }
 
-    static void printMaxValue (int[] array) {
+    static void printMaxValue(int[] array) {
+
         int max = array[0];
         int maxIndex = 0;
         int comparisions = 0;
         int replacements = 0;
-        for (int i = 0; i < array.length; i++){
-            if(array[i]>max){
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
                 max = array[i];
                 maxIndex = i;
                 replacements++;
@@ -24,4 +28,29 @@ public class SearchInArray {
         System.out.println("Comparisons: " + comparisions);
         System.out.println("Replacements: " + replacements);
     }
+
+    static int binarySearch(int[] array, int key) {
+
+        int position = 0;
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right) {
+            int middle = (left + right) / 2;
+            if (array[middle] == key) {
+                System.out.println("Key found");
+                return middle;
+            }
+            if (array[middle] < key) {
+                System.out.println("cut left half");
+                left = middle + 1;
+            }
+            if (array[middle] > key) {
+                System.out.println("Cut right half");
+                right = middle - 1;
+            }
+        }
+        return -1;
+    }
+
 }
